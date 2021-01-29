65919fb1f49b70d9521757664abaf6542132813c
Evgeniya3010 <susskaya86@gmail.com>
2021-01-30 01:54:23 +0200

    clears all

--- src/js/refs.js
+++
@@ -1,8 +0,0 @@
-const refs = {
-  movieLibrary: document.querySelector('.js-movie-library'),
-  searchForm: document.querySelector('.js-search-form'),
-  inputField: document.querySelector('.js-input'),
-  searchIcon: document.querySelector('.fa-search'),
-};
-
-export default refs;
